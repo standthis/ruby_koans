669f4390036509b583df6e7fac5605fc7872d716
ulwhednar <ulwhednar@tuta.io>
2019-09-23 21:41:01 +0200

    triangle complete

--- test.rb
+++ test.rb
@@ -43,4 +43,44 @@ def hashtest
   puts hash[:two]
 end
 
-hashtest
+def strings
+  puts ?b == (?a + 1)
+end
+
+def sym
+  :c + :d
+end
+
+def global(a,b)
+  a + b 
+end
+
+def method_with_var_args(*args)
+  return args
+  5 + 2
+end
+
+
+class Myclass
+  def my_private_method(b="a")
+    "a secret" + b
+  end
+  #private :my_private_method
+  #self.mymethod
+
+end
+def method_with_keyword_arguments(one: 1, two: 'two')
+  [one, two]
+end
+def test_break_statement
+  i = 1
+  result = 1
+  while true
+    break unless i <= 10
+    result = result * i
+    i += 1
+  end
+  result
+end
+
+puts 10.times
