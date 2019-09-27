1ff1a3aa27f1fd2c48a230ab8bfb3c5ab65fd25e
standthis <standthis@protonmail.ch>
2019-09-27 19:56:32 +0200

    about_proxy_object_project complete

--- koans/about_proxy_object_project.rb
+++ koans/about_proxy_object_project.rb
@@ -13,9 +13,24 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_reader :messages
   def initialize(target_object)
     @object = target_object
     # ADD MORE CODE HERE
+    @messages = []
+  end
+
+  def called?(msg)
+    @messages.include?(msg)
+  end
+
+  def number_of_times_called(msg)
+    @messages.count(msg)
+  end
+
+  def method_missing(method_name, *args, &block)
+    @messages << method_name
+    @object.send(method_name, *args, &block)
   end
 
   # WRITE CODE HERE
